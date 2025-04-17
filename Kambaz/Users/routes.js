a3d8504e98a90e4f146e70110d434e864cc0a0fd
Caden Elghazal <cadenelg@gmail.com>
2025-04-17 18:32:32 -0400

    readd error handling

--- Kambaz/Users/routes.js
+++ Kambaz/Users/routes.js
@@ -75,10 +75,10 @@ export default function UserRoutes(app) {
 
   const findCoursesForUser = async (req, res) => {
     const currentUser = req.session["currentUser"];
-    // if (!currentUser) {
-    //   res.sendStatus(401);
-    //   return;
-    // }
+    if (!currentUser) {
+      res.sendStatus(401);
+      return;
+    }
     if (currentUser.role === "ADMIN" || currentUser.role === "FACULTY") {
       const courses = await courseDao.findAllCourses();
       res.json(courses);
